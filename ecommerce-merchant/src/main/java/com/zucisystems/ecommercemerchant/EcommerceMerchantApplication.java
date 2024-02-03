c64e420c631bcb377f6e1eae99280043368f63a3
gokul101927 <gokullogan10@gmail.com>
2024-02-03 12:04:47 +0530

    first commit

---
+++ ecommerce-merchant/src/main/java/com/zucisystems/ecommercemerchant/EcommerceMerchantApplication.java
@@ -0,0 +1,13 @@
+package com.zucisystems.ecommercemerchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMerchantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMerchantApplication.class, args);
+	}
+
+}
---
+++ ecommerce-merchant/src/test/java/com/zucisystems/ecommercemerchant/EcommerceMerchantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zucisystems.ecommercemerchant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMerchantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
